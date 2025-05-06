ccfe4f649fe6bffd711c6ac03128ef09936fe687
Ruben <maxismaxis124@gmail.com>
2025-05-06 15:57:10 +0200

    update db

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,11 +1,20 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
+/*const pool = new Pool({
   user: 'postgres',
   password: '1234',
   host: 'localhost',
   port: 5432, // default Postgres port
   database: 'smash'
+});*/
+
+
+const pool = new Pool({
+  user: 'smash_m0lt_user',
+  password: 'SRl8NtfOd0cfdaWP19hpgiBQDIWyDUx3',
+  host: 'dpg-d08eu0c9c44c73btckj0-a.frankfurt-postgres.render.com',
+  port: 5432, // default Postgres port
+  database: 'smash_m0lt'
 });
 
 export function query(text: any): any {
