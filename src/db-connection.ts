ed597d2e9d9f5448c5095968a03bc659ce8ed982
Ruben <maxismaxis124@gmail.com>
2025-05-06 16:23:06 +0200

    supuesto arreglo de la base de datos

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -13,10 +13,19 @@ const pool = new Pool({
   user: 'smash_m0lt_user',
   password: 'SRl8NtfOd0cfdaWP19hpgiBQDIWyDUx3',
   host: 'dpg-d08eu0c9c44c73btckj0-a.frankfurt-postgres.render.com',
-  port: 5432, // default Postgres port
-  database: 'smash_m0lt'
+  port: 5432,
+  database: 'smash_m0lt',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+
+
+
+
+
+
 export function query(text: any): any {
-    return pool.query(text);
-};
+  return pool.query(text);
+}
